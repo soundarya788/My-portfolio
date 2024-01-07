67693f04489f0952a3cdc3c2089184d288bb20a7
soundarya788 <soundaryaa040@gmail.com>
2024-01-07 21:20:17 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
 const Home = () => (
-  <section id="home" style={{ backgroundColor: 'black', color: '#fff', marginTop: '100px',marginLeft:'20px' }}>
-    <div style={{marginTop:'100px',position:'absolute',marginLeft:'200px'}}>
-    <h1 style={{color:'orange',fontSize:'20px',fontFamily:'inherit'}}>Hello!</h1>
-    <p style={{fontSize:'60px',marginTop:'-15px'}}>I'm <span style={{color:'orange'}}>Soundarya </span>
-
-    </p>
-    <p style={{color:'orange',marginTop:'-90px',fontSize:'60px'}}>Priyadharshini </p>
-    <p style={{marginLeft:'100px',marginTop:'-60px',color:'grey'}}>- Frontend and Web developer</p>
+  <section
+    id="home"
+    style={{
+      backgroundColor: 'black',
+      color: '#fff',
+      marginTop: '100px',
+      marginLeft: '20px',
+      padding: '20px', // Add padding to improve spacing on small screens
+      textAlign: 'center', // Center content on small screens
+    }}
+  >
+    <div style={{ marginTop: '100px', position: 'absolute', marginLeft: '20px' }}>
+      <h1 style={{ color: 'orange', fontSize: '20px', fontFamily: 'inherit' }}>Hello!</h1>
+      <p style={{ fontSize: '40px', marginTop: '-15px' }}>I'm <span style={{ color: 'orange' }}>Soundarya </span></p>
+      <p style={{ color: 'orange', marginTop: '-60px', fontSize: '40px' }}>Priyadharshini </p>
+      <p style={{ marginLeft: '20px', marginTop: '-40px', color: 'grey', fontSize: '20px' }}>
+        - Frontend and Web developer
+      </p>
     </div>
-    
   </section>
 );
 
