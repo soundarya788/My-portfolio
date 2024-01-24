029c9c16a65984f7328ee94650bea002f49f01a7
soundarya788 <soundaryaa040@gmail.com>
2024-01-24 14:25:28 +0530

    update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,28 +1,31 @@
-
 import React from 'react';
 
-const About = () => (
-  <section id="about" style={{ backgroundColor: 'black', color: 'white', padding: '30px 20px', textAlign: 'center', marginLeft: '30px', marginTop: '240px' }}>
-    <h1 style={{ fontSize: '28px', marginBottom: '30px', borderBottom: '2px solid white', paddingBottom: '10px' }}>Skills</h1>
-    <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
-      <SkillProgressBar skill="HTML" />
-      <SkillProgressBar skill="CSS" />
-      <SkillProgressBar skill="JavaScript" />
-      <SkillProgressBar skill="React.js" />
-    </div>
-  </section>
-);
+function Skills() {
+  return (
+    <section id="skills" style={{ backgroundColor: 'black', color: 'white', padding: '50px 20px', textAlign: 'center',marginTop:'200px' }}>
+      <h1 style={{ fontSize: '28px', marginBottom: '20px', borderBottom: '2px solid white', paddingBottom: '10px' }}>Projects</h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center' }}>
+      <ProjectCard title="Resume " description="This React application for a personal CV. It includes two columns: the left column contains personal details, skills, language proficiency, and hobbies, while the right column features a career summary, work experience, educational background, and strengths. The layout is structured using HTML and styled with CSS." link="https://funny-torte-0705ed.netlify.app/" />
+        <ProjectCard title="UI design" description="This React application shows only for designs.The layout is structured using HTML and styled with CSS." link="https://gentle-unicorn-4c532b.netlify.app/" />
+      </div>
+    </section>
+  );
+}
 
-const SkillProgressBar = ({ skill, percentage }) => (
-  <div style={{ width: '100%', maxWidth: '500px', margin: '10px', textAlign: 'left' }}>
-    <p style={{ fontSize: '18px', marginBottom: '5px' }}>{skill}</p>
-    <div style={{ width: '100%', height: '10px', backgroundColor: '#333', borderRadius: '5px', overflow: 'hidden' }}>
-      <div style={{ width: `${percentage}%`, height: '100%', backgroundColor: 'orange', borderRadius: '5px', transition: 'width 1s ease-in-out' }}></div>
-    </div>
+const ProjectCard = ({ title, description, link }) => (
+  <div style={{ width: '100%', maxWidth: '500px', margin: '20px', padding: '20px', border: '1px solid white', borderRadius: '8px', textAlign: 'left' }}>
+    <h2 style={{ fontSize: '20px', marginBottom: '10px' }}>{title}</h2>
+    <p style={{ fontSize: '16px' }}>{description}</p>
+    <a href={link} target="_blank" rel="noopener noreferrer" style={{ color: 'orange', textDecoration: 'underline' }}>
+      Visit
+    </a>
   </div>
 );
 
-export default About;
+export default Skills;
+
+
+
 
 
 
